e1564fbee4e938485b8508393753838dbdd1637b
Aniket Sindhu <sindhuaniket@gmail.com>
2020-12-14 16:20:29 +0530

    homepage file made

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff171821),
+    );
+  }
+}
